37f7dc17a4acc4ea5587a1faeb7ba77b0556757a
xlmshack <xlmshack@gmail.com>
2018-07-26 23:56:12 +0800

    create a transfer task interface

---
+++ transfer_core/transfer_task.cpp
@@ -0,0 +1,17 @@
+#include "transfer_task.h"
+
+TransferTask::TransferTask() {
+
+}
+
+TransferTask::~TransferTask() {
+
+}
+
+void TransferTask::Start() {
+
+}
+
+void TransferTask::Cancel() {
+
+}
--- transfer_core/transfer_task.h
+++ transfer_core/transfer_task.h
@@ -1,8 +1,16 @@
 #ifndef KVNSFER_TRANSFER_CORE_TRANSFER_TASK_H_
 #define KVNSFER_TRANSFER_CORE_TRANSFER_TASK_H_
 
-class TransferTask {
+#include <wx/defs.h>
 
+class TransferTask {
+public:
+  TransferTask();
+  virtual ~TransferTask();
+  virtual void Start() = 0;
+  virtual void Cancel() = 0;
+
+  wxDECLARE_NO_COPY_CLASS(TransferTask);
 };
 
 #endif // KVNSFER_TRANSFER_CORE_TRANSFER_TASK_H_
